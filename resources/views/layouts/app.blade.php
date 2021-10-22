f0299fff3c12d924eea9c0efe9f4351a273c2741
arsnavneet <amit.khare588@gmail.com>
2021-10-22 11:00:43 +0530

    `commit`

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,21 +6,21 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{$meta->title}}</title>
         <meta name="description" content="{{$meta->description}}"/>
-        <meta property="og:url" content="https://www.amitkk.com{{$meta->url}}"/>
+        <meta property="og:url" content="https://www.lohewala.com{{$meta->url}}"/>
         <meta property="og:title" content="{{$meta->title}}"/>
         <meta property="og:description" content="{{$meta->description}}"/>
-        <meta property="og:image" content="https://www.amitkk.com{{$meta->image}}"/>
+        <meta property="og:image" content="https://www.lohewala.com{{$meta->image}}"/>
         <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:url" content="https://www.amitkk.com{{$meta->url}}"/>
+        <meta name="twitter:url" content="https://www.lohewala.com{{$meta->url}}"/>
         <meta name="twitter:title" content="{{$meta->title}}"/>
         <meta name="twitter:description" content="{{$meta->description}}"/>
-        <meta name="twitter:image" content="https://www.amitkk.com{{$meta->image}}"/>
-        <link rel="canonical" href="https://www.amitkk.com{{$meta->url}}"/>
-        <link rel="alternate" href="https://www.amitkk.com{{$meta->url}}" hreflang="x-default" />
-        <link rel="alternate" hreflang="en" href="https://www.amitkk.com{{$meta->url}}">
-        <link rel="preconnect" href="https://www.amitkk.com{{$meta->url}}" />
-        <link rel="dns-prefetch" href="https://www.amitkk.com{{$meta->url}}" />
-        <link rel="preload" as="image" href="https://www.amitkk.com{{$meta->image}}"/>
+        <meta name="twitter:image" content="https://www.lohewala.com{{$meta->image}}"/>
+        <link rel="canonical" href="https://www.lohewala.com{{$meta->url}}"/>
+        <link rel="alternate" href="https://www.lohewala.com{{$meta->url}}" hreflang="x-default" />
+        <link rel="alternate" hreflang="en" href="https://www.lohewala.com{{$meta->url}}">
+        <link rel="preconnect" href="https://www.lohewala.com{{$meta->url}}" />
+        <link rel="dns-prefetch" href="https://www.lohewala.com{{$meta->url}}" />
+        <link rel="preload" as="image" href="https://www.lohewala.com{{$meta->image}}"/>
         <meta name="allow-search" content="yes"/>
         <meta property="og:locale" content="en_US"/>
         <meta property="og:type" content="website"/>
